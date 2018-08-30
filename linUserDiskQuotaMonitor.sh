4d6d21d625aa9c5c866a653551b532f687dfe5b1
ramurn <uname.raja08@gmail.com>
2018-08-30 12:51:28 +0000

    master:ramurn:Added User Quota monitor Script

---
+++ linUserDiskQuotaMonitor.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Custom made script to check the disk quota used by particular user. 
+#This scipt was executed added in NagiosXI to capture the exit code and take action as per the exit code
+#Exit states are OK (0), WARN (1) and CRIT (2)
+
+TFILE=/tmp/val
+USER=username
+CHECK=`repquota -a | grep ${USER} | awk '{print $3/$5 * 100}' | awk '{printf("%d\n",$1 + 0.5)}'`
+
+rm ${TFILE}
+
+if (( ${CHECK} < 80 ));
+then
+echo "`quota ${USER} | sed -n '4p' | awk '{print $1}'` * 512 /1024/1024" | bc > ${TFILE}
+echo -e "OK: \c"; echo -e "Current Utilization quota of ${USER}: `cat ${TFILE}` MB";
+exit 0
+elif (( ${CHECK} < 90 ));
+then
+echo "`quota ${USER} | sed -n '4p' | awk '{print $1}'` * 512 /1024/1024" | bc > ${TFILE}
+echo -e "WARN: \c"; echo -e "Current Utilization quota of ${USER}: `cat ${TFILE}` MB";
+exit 1
+else
+echo "`quota ${USER} | sed -n '4p' | awk '{print $1}'` * 512 /1024/1024" | bc > ${TFILE}
+echo -e "CRIT: \c"; echo -e "Current Utilization quota of ${USER}: `cat ${TFILE}` MB";
+exit 2
+fi
+
